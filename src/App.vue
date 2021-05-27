f90fc04b92406c0ce30b4832f7c8f6c7588c1793
YOUR NAME <YOU@EXAMPLE.COM>
2021-05-27 09:59:31 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,12 @@
 </template>
 
 <script>
+@import "assets/css/base.css"
+
 import axios from 'axios'
+
 export default {
   name: 'App',
-  created(){
-    
-    axios.all([
-      axios ({
-        url: 'http://123.207.32.32:8000/home/multidata'
-      }),
-      axios ({
-        url: 'http://123.207.32.32/:8000/home/data',
-        params:{
-          type : 'sell',
-          page:5
-        }
-      })
-    ]).then( axios.spread((res1,res2) => {
-        console.log(res1);
-        console.log(res2);
-    }))
-  }
 }
 </script>
 
